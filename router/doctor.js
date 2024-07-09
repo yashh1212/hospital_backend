11981414a4a3627b4e534604825357e7eae2373c
yashh1212 <146187969+yashh1212@users.noreply.github.com>
2024-07-09 13:38:51 +0530

    Update doctor.js

--- router/doctor.js
+++ router/doctor.js
@@ -7,8 +7,7 @@ const router = express.Router();
 
 router.get("/search_doctor", checklogin, async (req, res) => {
   const { doctor_name, speciality } = req.query;
-  console.log(speciality);
-  try {
+   try {
     const doctor = await DoctorModel.find({
       $or: [{ doctor_name }, { speciality }],
     });
@@ -26,8 +25,7 @@ router.get("/doctor_information", checklogin, async (req, res) => {
   const dct_id = req.query.doctor_id;
   try {
     const doctor = await DoctorModel.findOne({ _id: dct_id });
-    console.log(doctor);
-    if (doctor.length <= 0) {
+     if (doctor.length <= 0) {
       return res.status(404).json("doctor not found");
     }
     return res.status(200).json(doctor);
@@ -40,8 +38,7 @@ router.get("/doctor_information", checklogin, async (req, res) => {
 router.get("/doctot-list", checklogin_hospital, async (req, res) => {
   try {
     const doctors = await DoctorModel.find({});
-    console.log(doctors);
-    if (!doctors) {
+     if (!doctors) {
       return res.status(401).json("data not found");
     }
     return res.status(200).json(doctors);
@@ -53,11 +50,9 @@ router.get("/doctot-list", checklogin_hospital, async (req, res) => {
 
 router.get("/speciality_search", checklogin,async (req, res) => {
   const speciality = req.query.speciality;
-  console.log(speciality)
-  try {
+   try {
     const doctors=await DoctorModel.find({speciality})
-    console.log(doctors)
-    if (doctors.length > 0){
+     if (doctors.length > 0){
       return res.status(200).json(doctors)
     }
      return res.status(404).json('not found');
