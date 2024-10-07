4b5200005ec0fa059ffc93a5a7ae4a3009d50387
Yash Dhokane <146187969+yashh1212@users.noreply.github.com>
2024-10-07 21:49:35 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const connecttodb=async()=>{
     mongoose.connect(
-      
+      "mongodb+srv://yashdhokane12:5ncHLAPanNO7cjLQ@hospital.hxp5pet.mongodb.net/urlshortener?retryWrites=true&w=majority&appName=hospital/urlshortener"
     );
 }
 
