568ba036ef280f197dc1df924ed1b3a61873f1d0
Yash Dhokane <146187969+yashh1212@users.noreply.github.com>
2025-01-02 23:15:25 +0530

    url chnages

--- connection.js
+++ connection.js
@@ -1,9 +1,7 @@
 const { default: mongoose } = require("mongoose");
-
+const url=process.env.url;
 const connecttodb=async()=>{
-    mongoose.connect(
-      "yash"
-    );
+    mongoose.connect(url);
 }
 
 module.exports=connecttodb;
